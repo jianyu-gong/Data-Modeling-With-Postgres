4eeba0f14e5575f293b3d186c6b2596938193ccb
Jianyu Gong <jianyu0503@gmail.com>
2019-05-22 19:05:09 -0400

    Adding docstrings
    add docstrings for each function in create_tables.py and etl.py

--- create_tables.py
+++ create_tables.py
@@ -3,6 +3,12 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def create_database():
+    """
+    Used to delete pre-existing database to ensure that our database does not throw any error if we try creating a
+    database that already exists. Then create a database called sparkifydb and connect to sparkifydb.
+    :return: cur: database cursor
+             conn: connection to studentdb database
+    """
     # connect to default database
     conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
     conn.set_session(autocommit=True)
@@ -23,12 +29,25 @@ def create_database():
 
 
 def drop_tables(cur, conn):
+    """
+    Used to delete pre-existing table to ensure that our database does not throw any error if we try creating a
+    table that already exists.
+    :param cur: cursor;
+    :param conn: connection;
+    :return: None
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Create tables based on the queries in create_table_queries list.
+    :param cur: cursor;
+    :param conn: connection; connection to studentdb database
+    :return: None
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
--- etl.py
+++ etl.py
@@ -6,22 +6,38 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    """
+    This function is used to read the song files and extract the song and artist information then insert into song
+    and artist tables.
+    :param cur: cursor; sparkifydb
+    :param filepath: str; the location of song file
+    :return: none
+    """
     # open song file
     df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = [df.song_id.values.tolist()[0], df.title.values.tolist()[0], \
-             df.artist_id.values.tolist()[0], df.year.values.tolist()[0], df.duration.values.tolist()[0]]
+    song_data = [df.song_id.values.tolist()[0], df.title.values.tolist()[0],
+                 df.artist_id.values.tolist()[0], df.year.values.tolist()[0], df.duration.values.tolist()[0]]
     print(song_data)
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = [df.artist_id.values.tolist()[0], df.artist_name.values.tolist()[0], \
-                   df.artist_location.values.tolist()[0], df.artist_latitude.values.tolist()[0], df.artist_longitude.values.tolist()[0]]
+    artist_data = [df.artist_id.values.tolist()[0], df.artist_name.values.tolist()[0],
+                   df.artist_location.values.tolist()[0], df.artist_latitude.values.tolist()[0],
+                   df.artist_longitude.values.tolist()[0]]
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
+    """
+    This function is used to read the log files and extract the user activities information then insert into time, user
+    and song played tables.
+    :param cur: cursor; sparkifydb
+    :param filepath: str; the location of log file
+    :return: none
+    """
+
     # open log file
     df = pd.read_json(filepath, lines=True)
 
@@ -34,23 +50,23 @@ def process_log_file(cur, filepath):
     # insert time data records
     time_data = (t, t.dt.hour, t.dt.day, t.dt.week, t.dt.month, t.dt.year, t.dt.weekday)
     column_labels = ("start_time", "hour", "day", "week", "month", "year", "weekday")
-    time_df = pd.DataFrame.from_dict({"start_time":t, 
-                                      "hour":t.dt.hour,
-                                      "day":t.dt.day,
-                                      "week":t.dt.dayofweek,
-                                      "month":t.dt.month,
-                                      "year":t.dt.year,
-                                      "weekday":t.dt.weekday})
+    time_df = pd.DataFrame.from_dict({"start_time": t,
+                                      "hour": t.dt.hour,
+                                      "day": t.dt.day,
+                                      "week": t.dt.dayofweek,
+                                      "month": t.dt.month,
+                                      "year": t.dt.year,
+                                      "weekday": t.dt.weekday})
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = pd.DataFrame.from_dict({"user_id":df['userId'], 
-                                      "first_name":df['firstName'],
-                                      "last_name":df['lastName'],
-                                      "gender":df['gender'],
-                                      "level":df['level']})
+    user_df = pd.DataFrame.from_dict({"user_id": df['userId'],
+                                      "first_name": df['firstName'],
+                                      "last_name": df['lastName'],
+                                      "gender": df['gender'],
+                                      "level": df['level']})
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -70,17 +86,26 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = (index, pd.to_datetime(row.ts, unit='ms'), \
+        songplay_data = (index, pd.to_datetime(row.ts, unit='ms'),
                          row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
+    """
+    This function is used to iterate all the files under filepath folder and process using a specified function.
+    :param cur: cursor
+    :param conn: connect; connect to specified database
+    :param filepath: str; location of the data file
+    :param func: function; the function used to process the data
+    :return: none
+    """
+
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
+        files = glob.glob(os.path.join(root, '*.json'))
+        for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
